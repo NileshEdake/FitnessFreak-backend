23346795853f6092aba90b3f885a5b960ce6f726
NileshEdake <nileshedake45@gmail.com>
2025-06-11 12:40:52 +0530

    Replaced sharp with jimp for easier deployment

--- Routes/imageUploadRoutes.js
+++ Routes/imageUploadRoutes.js
@@ -3,14 +3,13 @@ const router = express.Router();
 require('dotenv').config();
 const cloudinary = require('cloudinary').v2;
 const multer = require('multer');
-const sharp = require('sharp');
+const Jimp = require('jimp');
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET
 });
-
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
@@ -21,22 +20,25 @@ router.post('/uploadimage', upload.single('myimage'), async (req, res) => {
         return res.status(400).json({ ok: false, error: 'No image file provided' });
     }
 
-    sharp(file.buffer)
-        .resize({ width: 800 })
-        .toBuffer(async (err, data, info) => {
-            if (err) {
-                console.error('Image processing error:', err);
-                return res.status(500).json({ ok: false, error: 'Error processing image' });
+    try {
+        const image = await Jimp.read(file.buffer);
+        image.resize(800, Jimp.AUTO); // Resize width to 800 and auto-scale height
+
+        const buffer = await image.getBufferAsync(Jimp.MIME_JPEG);
+
+        cloudinary.uploader.upload_stream({ resource_type: 'image' }, (error, result) => {
+            if (error) {
+                console.error('Cloudinary Upload Error:', error);
+                return res.status(500).json({ ok: false, error: 'Error uploading image to Cloudinary' });
             }
 
-            cloudinary.uploader.upload_stream({ resource_type: 'auto' }, async (error, result) => {
-                if (error) {
-                    console.error('Cloudinary Upload Error:', error);
-                    return res.status(500).json({ ok: false, error: 'Error uploading image to Cloudinary' });
-                }
+            res.json({ ok: true, imageUrl: result.url, message: 'Image uploaded successfully' });
+        }).end(buffer);
 
-                res.json({ ok: true, imageUrl: result.url, message: 'Image uploaded successfully' });
-            }).end(data);
-        })
+    } catch (err) {
+        console.error('Image processing error:', err);
+        return res.status(500).json({ ok: false, error: 'Error processing image' });
+    }
 });
+
 module.exports = router;
