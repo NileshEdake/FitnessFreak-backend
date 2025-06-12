446afdcc093d75c8f079398ed666add74ca5479c
NileshEdake <nileshedake45@gmail.com>
2025-06-12 13:53:37 +0530

    Updated backend logic for fitness project

--- Middlewares/checkAuthToken.js
+++ Middlewares/checkAuthToken.js
@@ -4,28 +4,14 @@ function checkAuth(req, res, next) {
     const cookieToken = req.cookies.authToken;
     const headerToken = req.headers.authorization?.split(" ")[1]; // 👈 support Authorization header
     const authToken = cookieToken || headerToken; // 👈 choose one
-    const refreshToken = req.cookies.refreshToken;
 
-    if (!authToken || !refreshToken) {
-        return res.status(401).json({ message: 'Authentication failed: No authToken or refreshToken provided', ok: false });
+    if (!authToken) {
+        return res.status(401).json({ message: 'Authentication failed: No token provided', ok: false });
     }
 
     jwt.verify(authToken, process.env.JWT_SECRET_KEY, (err, decoded) => {
         if (err) {
-            jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (refreshErr, refreshDecoded) => {
-                if (refreshErr) {
-                    return res.status(401).json({ message: 'Authentication failed: Both tokens are invalid', ok: false });
-                } else {
-                    const newAuthToken = jwt.sign({ userId: refreshDecoded.userId }, process.env.JWT_SECRET_KEY, { expiresIn: '10m' });
-                    const newRefreshToken = jwt.sign({ userId: refreshDecoded.userId }, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '10d' });
-
-                    res.cookie('authToken', newAuthToken, { httpOnly: true });
-                    res.cookie('refreshToken', newRefreshToken, { httpOnly: true });
-
-                    req.userId = refreshDecoded.userId;
-                    next();
-                }
-            });
+            return res.status(401).json({ message: 'Authentication failed: Invalid token', ok: false });
         } else {
             req.userId = decoded.userId;
             next();
